be2c1212142eb171ca7f76b3bf01c26067b08e67
Holger Schmalle <h.schmalle@web.de>
2018-09-23 07:02:27 +0200

    Created USharp EditorFeatures project.

---
+++ src/EditorFeatures/USharp/Highlighting/KeywordHighlighters/UsingStatementHighlighter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Editor;
+using Microsoft.CodeAnalysis.Editor.Implementation.Highlighting;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Microsoft.CodeAnalysis.USharp.EditorFeatures.Highlighting.KeywordHighlighters
+{
+    [ExportHighlighter(LanguageNames.USharp)]
+    internal class UsingStatementHighlighter : AbstractKeywordHighlighter<UsingStatementSyntax>
+    {
+        protected override IEnumerable<TextSpan> GetHighlights(
+            UsingStatementSyntax usingStatement, CancellationToken cancellationToken)
+        {
+            yield return usingStatement.UsingKeyword.Span;
+        }
+    }
+}
