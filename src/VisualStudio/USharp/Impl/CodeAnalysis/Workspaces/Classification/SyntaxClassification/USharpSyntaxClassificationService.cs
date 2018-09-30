3f5c1ec8cbb276f97576c7eed035d675670a6a12
Holger Schmalle <h.schmalle@web.de>
2018-09-30 13:31:58 +0200

    Working on syntax classification.

---
+++ src/VisualStudio/USharp/Impl/CodeAnalysis/Workspaces/Classification/SyntaxClassification/USharpSyntaxClassificationService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Classification;
+using Microsoft.CodeAnalysis.Classification.Classifiers;
+using Microsoft.CodeAnalysis.CSharp.Classification;
+using Microsoft.CodeAnalysis.CSharp.Classification.Classifiers;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.CodeAnalysis.PooledObjects;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Microsoft.VisualStudio.LanguageServices.USharp.CodeAnalysis.Workspaces.Classification.SyntaxClassification
+{
+    [ExportLanguageService(typeof(ISyntaxClassificationService), LanguageNames.USharp), Shared]
+    internal class USharpSyntaxClassificationService : AbstractSyntaxClassificationService
+    {
+        private readonly ImmutableArray<ISyntaxClassifier> s_defaultSyntaxClassifiers =
+            ImmutableArray.Create<ISyntaxClassifier>(
+                new EmbeddedLanguagesTokenClassifier(),
+                new NameSyntaxClassifier(),
+                new SyntaxTokenClassifier(),
+                new UsingDirectiveSyntaxClassifier());
+
+        public override ImmutableArray<ISyntaxClassifier> GetDefaultSyntaxClassifiers()
+            => s_defaultSyntaxClassifiers;
+
+        public override void AddLexicalClassifications(SourceText text, TextSpan textSpan, ArrayBuilder<ClassifiedSpan> result, CancellationToken cancellationToken)
+            => ClassificationHelpers.AddLexicalClassifications(text, textSpan, result, cancellationToken);
+
+        public override void AddSyntacticClassifications(SyntaxTree syntaxTree, TextSpan textSpan, ArrayBuilder<ClassifiedSpan> result, CancellationToken cancellationToken)
+            => Worker.CollectClassifiedSpans(syntaxTree.GetRoot(cancellationToken), textSpan, result, cancellationToken);
+
+        public override ClassifiedSpan FixClassification(SourceText rawText, ClassifiedSpan classifiedSpan)
+            => ClassificationHelpers.AdjustStaleClassification(rawText, classifiedSpan);
+    }
+}
---
+++ src/VisualStudio/USharp/Impl/CodeAnalysis/Workspaces/Classification/USharpClassificationService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Classification;
+using Microsoft.CodeAnalysis.Host.Mef;
+using Microsoft.CodeAnalysis.PooledObjects;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Microsoft.VisualStudio.LanguageServices.USharp.CodeAnalysis.Workspaces.Classification
+{
+    [ExportLanguageService(typeof(IClassificationService), LanguageNames.CSharp), Shared]
+    internal class CSharpEditorClassificationService : AbstractClassificationService
+    {
+        public override void AddLexicalClassifications(SourceText text, TextSpan textSpan, List<ClassifiedSpan> result, CancellationToken cancellationToken)
+        {
+            var temp = ArrayBuilder<ClassifiedSpan>.GetInstance();
+            ClassificationHelpers.AddLexicalClassifications(text, textSpan, temp, cancellationToken);
+            AddRange(temp, result);
+            temp.Free();
+        }
+
+        public override ClassifiedSpan AdjustStaleClassification(SourceText text, ClassifiedSpan classifiedSpan)
+            => ClassificationHelpers.AdjustStaleClassification(text, classifiedSpan);
+    }
+}
