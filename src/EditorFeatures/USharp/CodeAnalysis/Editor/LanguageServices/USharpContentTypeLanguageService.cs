272d0bd8685d1fbe71a216b9a35ffc0ef85acc09
Holger Schmalle <h.schmalle@web.de>
2018-10-08 15:36:42 +0200

    Trying to enable usharp syntactic parser.

---
+++ src/EditorFeatures/USharp/CodeAnalysis/Editor/LanguageServices/USharpContentTypeLanguageService.cs
@@ -0,0 +1,22 @@
+﻿using System.Composition;
+using Microsoft.VisualStudio.Utilities;
+
+namespace Microsoft.CodeAnalysis.Editor.USharp.CodeAnalysis.Editor.LanguageServices
+{
+    [ExportContentTypeLanguageService(ContentTypeNames.USharpContentType, LanguageNames.USharp), Shared]
+    internal class USharpContentTypeLanguageService : IContentTypeLanguageService
+    {
+        private readonly IContentTypeRegistryService _contentTypeRegistry;
+
+        [ImportingConstructor]
+        public USharpContentTypeLanguageService(IContentTypeRegistryService contentTypeRegistry)
+        {
+            _contentTypeRegistry = contentTypeRegistry;
+        }
+
+        public IContentType GetDefaultContentType()
+        {
+            return _contentTypeRegistry.GetContentType(ContentTypeNames.CSharpContentType);
+        }
+    }
+}
--- src/EditorFeatures/USharp/CodeAnalysis/Workspaces/Classification/USharpClassificationService.cs
+++ src/EditorFeatures/USharp/CodeAnalysis/Workspaces/Classification/USharpClassificationService.cs
@@ -7,6 +7,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Classification;
+using Microsoft.CodeAnalysis.Extensions;
 using Microsoft.CodeAnalysis.Host.Mef;
 using Microsoft.CodeAnalysis.PooledObjects;
 using Microsoft.CodeAnalysis.Text;
@@ -14,9 +15,9 @@ using Microsoft.CodeAnalysis.Text;
 namespace Microsoft.VisualStudio.LanguageServices.USharp.CodeAnalysis.Workspaces.Classification
 {
     [ExportLanguageService(typeof(IClassificationService), LanguageNames.USharp), Shared]
-    internal class USharpEditorClassificationService : AbstractClassificationService
+    internal class USharpEditorClassificationService : IClassificationService 
     {
-        public override void AddLexicalClassifications(SourceText text, TextSpan textSpan, List<ClassifiedSpan> result, CancellationToken cancellationToken)
+        public void AddLexicalClassifications(SourceText text, TextSpan textSpan, List<ClassifiedSpan> result, CancellationToken cancellationToken)
         {
             var temp = ArrayBuilder<ClassifiedSpan>.GetInstance();
             ClassificationHelpers.AddLexicalClassifications(text, textSpan, temp, cancellationToken);
@@ -24,7 +25,42 @@ namespace Microsoft.VisualStudio.LanguageServices.USharp.CodeAnalysis.Workspaces
             temp.Free();
         }
 
-        public override ClassifiedSpan AdjustStaleClassification(SourceText text, ClassifiedSpan classifiedSpan)
+        public ClassifiedSpan AdjustStaleClassification(SourceText text, ClassifiedSpan classifiedSpan)
             => ClassificationHelpers.AdjustStaleClassification(text, classifiedSpan);
+
+        public async Task AddSemanticClassificationsAsync(Document document, TextSpan textSpan, List<ClassifiedSpan> result, CancellationToken cancellationToken)
+        {
+            var classificationService = document.Project.Solution.Workspace.Services.GetLanguageServices(LanguageNames.USharp).GetService<ISyntaxClassificationService>();
+
+            var extensionManager = document.Project.Solution.Workspace.Services.GetService<IExtensionManager>();
+            var classifiers = classificationService.GetDefaultSyntaxClassifiers();
+
+            var getNodeClassifiers = extensionManager.CreateNodeExtensionGetter(classifiers, c => c.SyntaxNodeTypes);
+            var getTokenClassifiers = extensionManager.CreateTokenExtensionGetter(classifiers, c => c.SyntaxTokenKinds);
+
+            var temp = ArrayBuilder<ClassifiedSpan>.GetInstance();
+            await classificationService.AddSemanticClassificationsAsync(document, textSpan, getNodeClassifiers, getTokenClassifiers, temp, cancellationToken).ConfigureAwait(false);
+            AddRange(temp, result);
+            temp.Free();
+        }
+
+        public async Task AddSyntacticClassificationsAsync(Document document, TextSpan textSpan, List<ClassifiedSpan> result, CancellationToken cancellationToken)
+        {
+            var classificationService = document.Project.Solution.Workspace.Services.GetLanguageServices(LanguageNames.USharp).GetService<ISyntaxClassificationService>();
+            var syntaxTree = await document.GetSyntaxTreeAsync(cancellationToken).ConfigureAwait(false);
+
+            var temp = ArrayBuilder<ClassifiedSpan>.GetInstance();
+            classificationService.AddSyntacticClassifications(syntaxTree, textSpan, temp, cancellationToken);
+            AddRange(temp, result);
+            temp.Free();
+        }
+
+        protected void AddRange(ArrayBuilder<ClassifiedSpan> temp, List<ClassifiedSpan> result)
+        {
+            foreach (var span in temp)
+            {
+                result.Add(span);
+            }
+        }
     }
 }
